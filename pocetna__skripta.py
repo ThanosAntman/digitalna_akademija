28a5eeb88d9095a3aa3fb86b753e1a1f9793a613
ThanosAntman <49249120+ThanosAntman@users.noreply.github.com>
2019-04-03 20:28:22 +0200

    dodavanje pocetne skripte

---
+++ pocetna__skripta.py
@@ -0,0 +1,26 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=input("ukucaj prvi broj")
+ukucaj prvi broj
+>>> 
+ RESTART: C:/Users/Digitalni Ninja/AppData/Local/Programs/Python/Python37-32/pocetna_skripta.py 
+ukucaj prvi broj9
+ukucaj drugi broj3
+zbrajanje 12
+oduzimanje 6
+mnozenje 27
+dijeljenje 3.0
+>>> 
+>>> 
+>>> 
+>>> 
+ RESTART: C:/Users/Digitalni Ninja/AppData/Local/Programs/Python/Python37-32/pocetna_skripta.py 
+ukucaj prvi broj5
+ukucaj drugi broj7
+zbrajanje 12
+oduzimanje -2
+mnozenje 35
+dijeljenje 0.7142857142857143
+>>> 
+ RESTART: C:/Users/Digitalni Ninja/AppData/Local/Programs/Python/Python37-32/pocetna_skripta.py 
+ukucaj prvi broj
